f4514049fec23b852cb541c565d3221a86e0e2f8
NterChoi <choi16289@naver.com>
2024-08-23 09:25:35 +0900

    첫 커밋

---
+++ game.js
@@ -0,0 +1,92 @@
+import chalk from 'chalk';
+import readlineSync from 'readline-sync';
+
+class Player {
+    constructor() {
+        this.hp = 100;
+        this.atk = 5;
+
+    }
+
+    attack() {
+
+    }
+}
+
+class Monster {
+    constructor() {
+        this.hp = 10;
+        this.atk = 2.5;
+    }
+
+    attack() {
+        // 몬스터의 공격
+    }
+}
+
+function displayStatus(stage, player, monster) {
+    console.log(chalk.magentaBright(`\n=== Current Status ===`));
+    console.log(
+        chalk.cyanBright(`| Stage: ${stage} `) +
+        chalk.blueBright(
+            `| Player HP : ${player.hp}, ATK : ${player.atk} |`
+        ) +
+        chalk.redBright(
+            `| Monster HP : ${monster.hp}, ATK : ${monster.atk}|`,
+        ),
+    );
+    console.log(chalk.magentaBright(`=====================\n`));
+}
+
+const battle = async (stage, player, monster) => {
+    let logs = [];
+
+    while(player.hp > 0) {
+        console.clear();
+        displayStatus(stage, player, monster);
+
+        logs.forEach((log) => console.log(log));
+
+        console.log(
+            chalk.green(
+                `\n1. 공격한다 2. 아무것도 하지않는다.`,
+            ),
+        );
+        const choice = readlineSync.question('당신의 선택은? ');
+
+        // 플레이어의 선택에 따라 다음 행동 처리
+        logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+
+        switch (choice) {
+            case '1':
+                console.log(chalk.green('게임을 시작합니다.'));
+                // 여기에서 새로운 게임 시작 로직을 구현
+                startGame();
+                break;
+            case '2':
+                console.log(chalk.yellow('구현 준비중입니다.. 게임을 시작하세요'));
+                // 업적 확인하기 로직을 구현
+                break;
+            default:
+                console.log(chalk.red(`올바른 선택을 하세요.`));
+        }
+
+
+    }
+
+};
+
+export async function startGame() {
+    console.clear();
+    const player = new Player();
+    let stage = 1;
+
+    while (stage <= 10) {
+        const monster = new Monster(stage);
+        await battle(stage, player, monster);
+
+        // 스테이지 클리어 및 게임 종료 조건
+
+        stage++;
+    }
+}
